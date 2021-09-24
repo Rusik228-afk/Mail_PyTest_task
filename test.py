0dea00034280205f80a684da02cfb2ef2c9a33d0
ruslan.alckaev@yandex.ru <ruslan.alckaev@yandex.ru>
2021-09-24 13:48:42 +0300

    TestDict

--- test.py
+++ test.py
@@ -25,4 +25,33 @@ class TestTuple:
             assert t[1] == 200 # assignment was not 
     
 
-        
+class TestDict:
+    def test_equality(self):
+        d1 = {
+            'a': 100500,
+            True: [.2, .75, .99]
+        }
+        d2 = {
+            True: [.2, .75, .99],
+            'a': 100500
+        }
+        assert d1 == d2
+
+    @pytest.mark.parametrize(
+        'dict_contents, expected',
+        [
+            (zip(['key1', 'key2', 'key3'], range(3)), {'key1': 0, 'key2': 1, 'key3': 2}),
+            ([('galaktionov', 'english'*2), ('gladyshev', 'oscilloscope')], {'gladyshev': 'oscilloscope', 'galaktionov': 'englishenglish'})
+        ]
+    )
+    def test_creation(self, dict_contents, expected):
+        assert dict(dict_contents) == expected
+
+    def test_comparison(self):
+        d = {
+            'key': ['v', 'a', 'l', 'u', 'e']
+        }
+        try:
+            assert d < {'key': ['v', 'a', 'l', 'u', 'e']}
+        except TypeError:
+            pass        
